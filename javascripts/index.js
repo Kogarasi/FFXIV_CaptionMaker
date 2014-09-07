1f348138610f0790d50fc05c12a36140cc41e053
kogarasi <kogarasi.cross@gmail.com>
2014-09-07 14:29:26 +0900

    Build 2014/09/07 14:29

---
+++ javascripts/index.js
@@ -0,0 +1,80 @@
+var CaptionMaker;
+(function (CaptionMaker) {
+    var Font = (function () {
+        function Font() {
+        }
+        Font.prototype.render = function (context) {
+            context.font = this.size + "px " + this.name;
+            context.fillStyle = this.color;
+            context.fillText(this.text, this.x, this.y);
+        };
+        return Font;
+    })();
+    CaptionMaker.Font = Font;
+})(CaptionMaker || (CaptionMaker = {}));
+var CaptionMaker;
+(function (CaptionMaker) {
+    var Image = (function () {
+        function Image() {
+            this.placeholder = document.createElement("img");
+        }
+        Object.defineProperty(Image.prototype, "src", {
+            set: function (_src) {
+                this.placeholder.src = _src;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        Image.prototype.render = function (context) {
+            context.drawImage(this.placeholder, 0, 0);
+        };
+        return Image;
+    })();
+    CaptionMaker.Image = Image;
+})(CaptionMaker || (CaptionMaker = {}));
+var CaptionMaker;
+(function (CaptionMaker) {
+    var Renderer = (function () {
+        function Renderer(canvas) {
+            this.context = canvas.getContext('2d');
+            this.width = canvas.width;
+            this.height = canvas.height;
+        }
+        Renderer.prototype.render = function (callback) {
+            var _this = this;
+            var func = function () {
+                _this.context.clearRect(0, 0, _this.width, _this.height);
+                callback(_this.context);
+
+                requestAnimationFrame(func);
+            };
+
+            func();
+        };
+        return Renderer;
+    })();
+    CaptionMaker.Renderer = Renderer;
+})(CaptionMaker || (CaptionMaker = {}));
+angular.module('CaptionMaker', []).controller('CaptionMakerController', [
+    '$scope', function ($scope) {
+        var canvas = $("#canvas")[0];
+        $scope.renderer = new CaptionMaker.Renderer(canvas);
+        $scope.image = new CaptionMaker.Image();
+        $scope.font = new CaptionMaker.Font();
+
+        $scope.image.src = 'images/caption0.jpg';
+
+        $scope.font.text = "test";
+        $scope.font.size = 20;
+        $scope.font.name = "Arial";
+        $scope.font.x = 20;
+        $scope.font.y = 80;
+
+        $scope.renderer.render(function (context) {
+            $scope.image.render(context);
+            $scope.font.render(context);
+        });
+    }]);
+//# sourceMappingURL=index.js.map
+;
